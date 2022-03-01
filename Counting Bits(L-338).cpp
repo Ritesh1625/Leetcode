3eb10dce22dcd3d39468fa7a3c3c34689bfc817f
Ritesh Mahato <87099919+Ritesh1625@users.noreply.github.com>
2022-03-01 21:08:35 +0530

    Create Counting Bits(L-338).cpp

---
+++ Counting Bits(L-338).cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector <int> ans;
+        
+        for(int i=0; i<=n; i++){
+            int sum = 0;
+            int num = i;
+            
+            while(num!=0){
+                sum += num%2;
+                num = num/2;    
+            }
+            ans.push_back(sum);
+        }
+        return ans;
+    }
+};
+
+/* Another solution */
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector <int> t(n+1);
+        
+        t[0] = 0;
+        
+        for(int i=0; i<=n; i++){
+            t[i] = t[i/2] + i%2;
+        
+        }
+        return t;
+    }
+};
